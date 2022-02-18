21e694dbbcc8fe69c84110be6518df26a080dc68
VitaliiVoloboiev <83081678+VitaliiVoloboiev@users.noreply.github.com>
2022-02-18 14:51:28 +0000

    Update aws-script.tf

--- aws-script.tf
+++ aws-script.tf
@@ -1,10 +1,10 @@
 provider "m3" {}
 
 resource "m3_script" "script_for_test" {
-  script_name = "aws-script"
-  script_content = "text content for 5364 aws script"
-  script_extension = ".cmd"
-  tenant_name = "SOFTLINE"
-  region_name = "AWS-USEAST-2"
-  cloud = "AWS"
+  script_name = "openStack-script"
+  script_content = "text content for 5364 OpenStack script"
+  script_extension = ".sh"
+  tenant_name = "MCS-SFTL"
+  region_name = "MCS-SFTL-R1"
+  cloud = "OPEN_STACK"
 }
